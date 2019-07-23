ca6df3c09b8f76ddf6e40af8e4ff63c3f49a2b57
lhuddlesto <lance299@gmail.com>
2019-07-22 23:34:06 -0400

    make use of port variable

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const app = express()
 app.get('/', (req, res) => res.download('./resume.pdf'))
 
 const port = process.env.PORT || 3000
-app.listen(3000, () => console.log(`App is listening on PORT ${port}`))
+app.listen(port, () => console.log(`App is listening on PORT ${port}`))
