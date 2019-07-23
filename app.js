11891e76aff338f9f8bb404a4db8809ebec4efaf
lhuddlesto <lance299@gmail.com>
2019-07-23 07:12:07 -0400

    added headers for CORS issues

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.download('./resume.pdf'))
+app.get('/', (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.download('./resume.pdf')
+})
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log(`App is listening on PORT ${port}`))
