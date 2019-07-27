be88e78272aaf57769929a0a694d50b3c7bb4846
lhuddlesto <lance299@gmail.com>
2019-07-27 18:49:24 -0400

    Email route

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+const sgMail = require('@sendgrid/mail')
+const cors = require('cors')
 
-app.get('/', (req, res) => {
+
+sgMail.setApiKey(process.env.LHUDDLESTO_SENDGRID_KEY)
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (res) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.download('./resume.pdf')
 })
 
-const port = process.env.PORT || 3000
+app.post('/email', (req, res) => {
+
+   console.log(req.body)
+    try {
+        if (!req.body.work || req.body.work === "null") {
+            return res.send("Your request could not be completed.  Please choose a valid work type.")
+        }
+        const msg = {
+            to: 'info@lhuddlesto.com',
+            from: req.body.email,
+            subject: `${req.body.fname} ${req.body.lname} - ${req.body.work}`,
+            text: req.body.message
+        }
+        sgMail.send(msg);
+        return res.send("Thank you!  Your email has been sent.")
+
+    } catch (e) {
+        console.log("Whoops")
+        res.send(e)
+    }
+
+})
+
+const port = process.env.PORT || 9000
 app.listen(port, () => console.log(`App is listening on PORT ${port}`))
